e8fbe89b0d42fe51aa4aa1ded423c9f4a525b981
ShanuDevCodes <shanu.dev.codes@gmail.com>
2026-04-01 17:31:01 +0530

    Initial commit

---
+++ src/main/java/com/shanudevcodes/fdpacb/FinanceDataProcessingAndAccessControlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shanudevcodes.fdpacb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceDataProcessingAndAccessControlBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceDataProcessingAndAccessControlBackendApplication.class, args);
+    }
+
+}
